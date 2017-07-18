528e608dbb83a1de5481b4fc58baa5d4d4ca78f7
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2017-07-18 09:51:35 +0100

    Changed the pattern for "inactive" status

--- blink1-status.sh
+++ blink1-status.sh
@@ -7,7 +7,7 @@ if [ "$test" == "active" ]; then
     /home/pi/blink1/commandline/blink1-tool --green --glimmer=10 -q
 elif [ "$test" == "inactive" ]; then
     #echo -e "inactive"
-    /home/pi/blink1/commandline/blink1-tool --red --running=10 -q
+    /home/pi/blink1/commandline/blink1-tool --red -q
 else
     #echo -e "unknown"
     /home/pi/blink1/commandline/blink1-tool --yellow -q
